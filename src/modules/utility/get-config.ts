0300259ed2c7c9707dd03d9dfde73f104988091b
John Pinkster <john.c.pinkster@gmail.com>
2019-10-03 16:03:41 -0400

    fix(config): make fallback optional

--- src/modules/utility/get-config.ts
+++ src/modules/utility/get-config.ts
@@ -1,13 +1,15 @@
 import * as config from 'config';
 
-export function getConfig<ConfigT>(key: string, fallback: ConfigT): ConfigT {
+export function getConfig<ConfigT>(key: string, fallback?: ConfigT): ConfigT {
 
     let result: ConfigT;
-
     try {
         result = config.get<ConfigT>(key);
     }
-    catch {
+    catch (error) {
+        if (fallback === undefined) {
+            throw error;
+        }
         result = fallback;
     }
 
