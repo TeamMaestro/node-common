e7a81f4a46bfdb49955be5d9885c46b4d4388125
Josh Gibson <gibsonjoshua55@gmail.com>
2019-10-03 15:23:10 -0400

    fix(utility): removes unnecessary param

--- src/modules/utility/merge-object-into-file.ts
+++ src/modules/utility/merge-object-into-file.ts
@@ -1,7 +1,7 @@
 import * as deepmerge from 'deepmerge';
 import * as path from 'path';
 
-export function mergeObjectIntoFile(currentDir: string, pathName: string, object, random) {
+export function mergeObjectIntoFile(currentDir: string, pathName: string, object) {
     const filePath = path.join(currentDir, pathName);
     let fileObj;
     try {
