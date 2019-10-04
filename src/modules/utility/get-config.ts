07bdb8373d0ded771d1bcde1d852aaafe9566edc
Josh Gibson <gibsonjoshua55@gmail.com>
2019-10-04 08:59:21 -0400

    fix(circular-dep): removes circular dependency when using getConfig

--- src/modules/utility/get-config.ts
+++ src/modules/utility/get-config.ts
@@ -1,10 +1,8 @@
-import * as config from 'config';
-
 export function getConfig<ConfigT>(key: string, fallback?: ConfigT): ConfigT {
-
+    const config = require('config');
     let result: ConfigT;
     try {
-        result = config.get<ConfigT>(key);
+        result = config.get(key);
     }
     catch (error) {
         if (fallback === undefined) {
