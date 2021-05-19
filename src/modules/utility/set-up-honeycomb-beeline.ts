46ddfe5cc4f76a5894ae1ac0b79dd489e64aafad
Josh Gibson <gibsonjoshua55@gmail.com>
2021-05-19 13:12:53 -0400

    feat(honeycomb): support ignored routes by default

--- src/modules/utility/set-up-honeycomb-beeline.ts
+++ src/modules/utility/set-up-honeycomb-beeline.ts
@@ -1,7 +1,38 @@
 import { BeelineOpts } from '@teamhive/honeycomb-beeline';
 import beeline = require('@teamhive/honeycomb-beeline');
+import deterministicSamplerFactory = require('@teamhive/honeycomb-beeline/lib/deterministic_sampler')
 
-export function setUpHoneycombBeeline(opts: BeelineOpts & {appendEnvToDataset: boolean}) {
+function sampleHookFactory(ignoredRoutes: string[]) {
+    return (data: any) => {
+        // default sample rate to 10
+        let sampleRate = 10;
+    
+        const deterministicSampler = deterministicSamplerFactory(sampleRate);
+
+        let shouldSample = deterministicSampler(data);
+
+        // drop ignoredRoutes
+        if (ignoredRoutes.includes(data["request.path"])) {
+            shouldSample = false;
+            sampleRate = 0;
+        }
+        return {
+            shouldSample,
+            sampleRate
+        }
+    }
+}
+
+
+export function setUpHoneycombBeeline(opts: BeelineOpts & {
+    appendEnvToDataset: boolean;
+    /**
+     * When no custom sampler hook is provided, these routes will be ignored
+     * defaults to /api/v1/versions
+     */
+    ignoredRoutes?: string[]
+}) {
+    const {ignoredRoutes = ['/api/v1/versions']} = opts;
     if (!opts.writeKey) {
         opts.writeKey = process.env.HONEYCOMB_WRITEKEY;
     }
@@ -11,6 +42,9 @@ export function setUpHoneycombBeeline(opts: BeelineOpts & {appendEnvToDataset: b
     }
     if (!opts.serviceName) {
         opts.serviceName = process.env.APP;
+    }
+    if (!opts.samplerHook) {
+        opts.samplerHook = sampleHookFactory(ignoredRoutes)
     }
     beeline(opts);
 }
