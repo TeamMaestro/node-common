af2ee32bb1be56ab8694fc2653f00859c39432c5
Josh Gibson <gibsonjoshua55@gmail.com>
2021-07-08 08:31:31 -0400

    fix(try-catch): create a trace if no trace exists otherwise use span

--- src/modules/try-catch/try-catch.decorator.ts
+++ src/modules/try-catch/try-catch.decorator.ts
@@ -15,48 +15,85 @@ export function TryCatch(optionsOrException = {} as TryCatchOptions | TryCatchEx
 
         // check if original methods is async
         if (!options.isSynchronous) {
-            descriptor.value = async function(...args: any[]) {
-                const {tryCatchOptions} = getTryCatchOptions(optionsOrException, options);
-                if (beelineEnabled && tryCatchOptions.createTrace) {
 
-                    return await beeline.startAsyncSpan(tryCatchOptions.createTrace, async (span) => {
-                        // try catch the original method passing in args it was called with
-                        try {
-                            return await originalMethod.apply(this, args);
-                        }
-                        catch (error) {
-                            catchError(error, optionsOrException, options);
-                        } finally {
-                                beeline.finishTrace(span);
-                        }
-                    })
-                } else {
+
+            descriptor.value = async function(...args: any[]) {
+                const runFunctionAsync = async (finallyFn?: () => void) => {
                     try {
                         return await originalMethod.apply(this, args);
                     }
                     catch (error) {
                         catchError(error, optionsOrException, options);
+                    } finally {
+                        if (finallyFn) {
+                            finallyFn()
+                        }
                     }
+                }
+
+                const {tryCatchOptions} = getTryCatchOptions(optionsOrException, options);
+
+                if (beelineEnabled && tryCatchOptions.createTrace) {
+
+                    // if there is no active trace, create one
+                    if (!beeline.getTraceContext()) {
+                        const trace = beeline.startTrace(tryCatchOptions.createTrace);
+                        return await runFunctionAsync(() => {
+                            beeline.finishSpan(trace);
+                        });
+                    }
+                    // if there is an active trace, add a span to the trace 
+                    else {
+                        return await beeline.startAsyncSpan(tryCatchOptions.createTrace, async (span) => {
+                            return await runFunctionAsync(() => {
+                                beeline.finishSpan(span);
+                            })
+                        })
+                    }
+                } 
+                // if tracing is not enabled, just run the function
+                else {
+                    return await runFunctionAsync();
                 }
             };
         }
         else {
             descriptor.value = function(...args: any[]) {
-                const {tryCatchOptions} = getTryCatchOptions(optionsOrException, options);
-                let span: beeline.Span;
-                if (tryCatchOptions.createTrace) {
-                    span = beeline.startSpan(tryCatchOptions.createTrace)
-                }
-                // try catch the original method passing in args it was called with
-                try {
-                    return originalMethod.apply(this, args);
-                }
-                catch (error) {
-                    catchError(error, optionsOrException, options);
-                } finally {
-                    if (span) {
-                        beeline.finishSpan(span);
+
+                const runFunction = (finallyFn?: () => void) => {
+                    try {
+                        return originalMethod.apply(this, args);
                     }
+                    catch (error) {
+                        catchError(error, optionsOrException, options);
+                    } finally {
+                        if (finallyFn) {
+                            finallyFn()
+                        }
+                    }
+                }
+
+                const {tryCatchOptions} = getTryCatchOptions(optionsOrException, options);
+                if (beelineEnabled && tryCatchOptions.createTrace) {
+
+                    // if there is no active trace, create one
+                    if (!beeline.getTraceContext()) {
+                        const trace = beeline.startTrace(tryCatchOptions.createTrace);
+                        return runFunction(() => {
+                            beeline.finishSpan(trace);
+                        });
+                    }
+                    // if there is an active trace, add a span to the trace 
+                    else {
+                        const span = beeline.startSpan()
+                        return runFunction(() => {
+                                beeline.finishSpan(span);
+                        });
+                    }
+                } 
+                // if tracing is not enabled, just run the function
+                else {
+                    return runFunction()
                 }
             };
         }
